20b5819d8958e479a453226c0bffa940c617fcac
YcaroMotta <145229770+YcaroMotta@users.noreply.github.com>
2025-03-08 23:11:42 -0300

    Add files via upload

---
+++ Questôes 20 a 30/Questão242.java
@@ -0,0 +1,58 @@
+/*
+ algoritmo "VerificaPertence"
+
+var
+    x, y, resultado: inteiro
+
+inicio
+
+    // Entrada de dados
+    escreva("Digite o valor de X: ")
+    leia(x)
+
+    escreva("Digite o valor de Y: ")
+    leia(y)
+
+    // Cálculo do resultado
+    resultado <- 2 * x + y
+
+    // Verificação se pertence
+    se resultado = 3 entao
+        escreva("PERTENCE")
+    senao
+        escreva("NAO PERTENCE")
+    fimse
+
+FIM
+Teste de Mesa
+X	Y	Resultado (2*X + Y)	Pertence?
+1	1	3	                PERTENCE
+2	2	6	                NAO PERTENCE
+0	3	3	                PERTENCE
+ */
+package questão24.pkg2;
+
+import javax.swing.JOptionPane;
+
+public class Questão242 {
+
+    public static void main(String[] args) {
+       int x,y,result;
+       String s_x, s_y;
+       
+       s_x = JOptionPane.showInputDialog(null, "Digite o valor de X");
+       x = Integer.parseInt(s_x);
+       s_y = JOptionPane.showInputDialog(null, "Digite o valor de Y");
+       y = Integer.parseInt(s_y);
+       
+       result = 2*x+y;
+          if(result==3)
+                 {
+                     JOptionPane.showMessageDialog(null, " PERTENCE ");
+                 }
+         else
+          {
+              JOptionPane.showMessageDialog(null, " NAO PERTENCE ");
+          }
+    }
+}
