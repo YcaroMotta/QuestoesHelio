38341639fa1beb705b5f23da70edd62a095986ac
YcaroMotta <145229770+YcaroMotta@users.noreply.github.com>
2025-03-08 23:09:36 -0300

    Questões 20 a 30

---
+++ Questão292.java
@@ -0,0 +1,56 @@
+/*
+INICIO
+
+VAR
+car;
+
+Escreva ->"Digite o Caractere"
+
+Se ( (car.equals("a")) || (car.equals("e")) || (car.equals("i")) || (car.equals("o")) || (car.equals("u")))
+     Escreva ->"O Caractere e uma Vogal"
+
+Se Senão ((car.equals("b")) ||(car.equals("c")) ||(car.equals("d")) ||(car.equals("f")) ||(car.equals("g")) ||(car.equals("h")) ||(car.equals("j")) ||
+         (car.equals("k")) ||(car.equals("l")) ||(car.equals("m")) ||(car.equals("n")) ||(car.equals("p")) ||(car.equals("q")) ||(car.equals("r")) ||(car.equals("s")) ||
+         (car.equals("t")) ||(car.equals("v")) ||(car.equals("w")) ||(car.equals("x")) ||(car.equals("y")) ||(car.equals("z")))
+     Escreva ->"O Caractere e uma Consoante"
+
+Senão
+     Escreva ->"O Caractere e Outro"
+
+FIM
+
+Teste de Messa
+Car Escreva
+A   Vogal
+D   Consoante
+I   Vogal
+*/
+package questão29.pkg2;
+
+import javax.swing.JOptionPane;
+
+public class Questão292 {
+
+    public static void main(String[] args) {
+        String car;
+        
+        car = JOptionPane.showInputDialog("Digite o Caractere");
+        
+        if ( (car.equals("a")) || (car.equals("e")) || (car.equals("i")) || (car.equals("o")) || (car.equals("u")))
+        {
+            JOptionPane.showMessageDialog(null, "O Caractere e uma Vogal");
+        }
+        else if ((car.equals("b")) ||(car.equals("c")) ||(car.equals("d")) ||(car.equals("f")) ||(car.equals("g")) ||(car.equals("h")) ||(car.equals("j")) ||
+                (car.equals("k")) ||(car.equals("l")) ||(car.equals("m")) ||(car.equals("n")) ||(car.equals("p")) ||(car.equals("q")) ||(car.equals("r")) ||(car.equals("s")) ||
+                (car.equals("t")) ||(car.equals("v")) ||(car.equals("w")) ||(car.equals("x")) ||(car.equals("y")) ||(car.equals("z")))
+        {
+            JOptionPane.showMessageDialog(null, "O Caractere e uma Consoante");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "O Caractere e Outro");
+        }
+
+    }
+    
+}
